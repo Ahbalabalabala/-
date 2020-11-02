892e84a8aa80c84d1d3e639a398cc39f96b13b83
Ahbalabalabala <7945509+ah_balabalabala@user.noreply.gitee.com>
2020-11-02 20:16:46 +0800

    map

--- src/views/around/Shopkeeper.vue
+++ src/views/around/Shopkeeper.vue
@@ -1,19 +1,7 @@
 <template>
     <div class="Shop">
         <van-nav-bar title="" left-text="返回" left-arrow :fixed="true" @click-left="backaround" />
-        <div>12456</div>
-        <button>aidnji</button>
-        <button>aidnji</button>
-        <button>aidnji</button>
-        <button>aidnji</button>
-        <button>aidnji</button>
-        <button>aidnji</button>
-        <button>aidnji</button>
-        <button>aidnji</button>
-        <p>418641856456165165</p>
-        <p>418641856456165165</p>
-        <p>418641856456165165</p>
-        <p>418641856456165165</p>
+       
     </div>
 </template>
 <script>
